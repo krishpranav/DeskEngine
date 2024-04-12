6eb77f17a00337ae3633221aee759d17f5cdbb31
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-12 12:09:58 +0530

    include: utils[typetraits]

---
+++ include/utils/TypeTag.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file TypeTag.hpp
+ * @author Krisna Pranav
+ * @brief typetag
+ * @version 1.0
+ * @date 2024-04-12
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav
+ *
+ */
+
+#pragma once
+
+#ifndef DESK_TYPETRAITS_HPP
+#define DESK_TYPETRAITS_HPP
+
+namespace Desk
+{
+    template <typename T>
+    struct TypeTag
+    {
+        using Type = T;
+    };
+} // namespace Desk
+
+#endif
